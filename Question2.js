6574b6c48b6cb82aaafc3fb2abdd9b86db419ba3
amosmugendi <amosmugendi07@gmail.com>
2024-04-01 23:01:28 +0300

    coomplete challenge

--- Question2.js
+++ Question2.js
@@ -1,9 +1,9 @@
 /*function declartion that takes two numbers as parameters */
 function arraygen(num1,num2){
     let nums=[] //deefine the array
-
+//use a loop to generate the numbers that start from the first number to the last number
     for (let i= num1; i <= num2; i++){
-        nums.push(i);
+        nums.push(i);//push the created numbers using the array method push 
     }
     return nums;
     // console.log(nums)
